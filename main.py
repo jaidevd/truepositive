b96e8ac9da638cf66a089e0147c6c5aca4337b76
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2015-02-13 13:38:32 +0530

    Functional fileDialog

--- main.py
+++ main.py
@@ -16,6 +16,20 @@ import os.path as op
 from PySide import QtCore, QtGui
 
 
+class MyFileDialog(QtGui.QFileDialog):
+
+    accepted = QtCore.Signal(QtCore.QObject)
+
+    def __init__(self, parent=None):
+        super(MyFileDialog, self).__init__()
+        self.parent = parent
+
+    def accept(self):
+        super(MyFileDialog, self).accept()
+        setattr(self.parent, "filepath", self.selectedFiles()[0])
+        self.accepted.emit(self.parent)
+
+
 class DataFrameModel(QtCore.QAbstractTableModel):
 
     def __init__(self, df=None):
@@ -48,16 +62,26 @@ class MainWindow(QtGui.QMainWindow):
     def __init__(self, filepath=None):
         super(MainWindow, self).__init__()
         self.filepath = filepath
-        self.read_csv()
-
         self.tableView = QtGui.QTableView(self)
-        self.dataFrameModel = DataFrameModel(self.df)
-
-        self.tableView.setModel(self.dataFrameModel)
         self.setCentralWidget(self.tableView)
+        self.readCsv()
 
-    def read_csv(self):
-        self.df = pd.read_csv(self.filepath)
+        # Menu Bar
+        self.openFileDialog = MyFileDialog(parent=self)
+        self.openFileDialog.accepted.connect(self.readCsv)
+
+        menuBar = QtGui.QMenuBar()
+        fileMenu = QtGui.QMenu("&File", parent=menuBar)
+        openAct = fileMenu.addAction("Open")
+        openAct.triggered.connect(self.openFileDialog.exec_)
+        menuBar.addMenu(fileMenu)
+
+        self.setMenuBar(menuBar)
+
+    def readCsv(self):
+        df = pd.read_csv(self.filepath)
+        self.dataFrameModel = DataFrameModel(df)
+        self.tableView.setModel(self.dataFrameModel)
 
 
 if __name__ == '__main__':
